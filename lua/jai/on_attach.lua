675f477667bb8cd42f46abbb1a5d0a833a4b7b08
John Arumemi-Ikhide <john.arumemi@gmail.com>
2023-02-05 19:33:30 +0000

    update keymap in lsp on_attach function
    updated the keymaps to be configured on attachment on a LSP server
    for the specific buffer. They were previously using global mappings.

--- lua/jai/on_attach.lua
+++ lua/jai/on_attach.lua
@@ -1,36 +1,55 @@
 -- [[ used in every lspconfig by default ]]
 
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 return function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- scope these keybinds to buffer for attached language server
+  -- below should only be passed 2 further arguments
+  -- NOTE: vim.keymap.set accepts direct lua functions in {rhs} argument.
+  -- Underneath, it calls vim.api.nvim_set_keymap approriately
+  local function buf_set_keymap(...) vim.keymap.set(...) end
+  -- below should only be passed 3 further arguments
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  local bufopts = { noremap=true, silent=true, buffer = bufnr }
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
+
+  -- Enable completion triggered by <c-x><c-o>
+  -- TODO: is this redundant with the use of nvim_cmp?
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  buf_set_keymap('n', '<space>e', vim.diagnostic.open_float, bufopts)
+  buf_set_keymap('n', '[d', vim.diagnostic.goto_prev, bufopts)
+  buf_set_keymap('n', ']d', vim.diagnostic.goto_next, bufopts)
+  buf_set_keymap('n', '<space>q', vim.diagnostic.setloclist, bufopts)
+
+  buf_set_keymap('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  buf_set_keymap('n', 'gd', vim.lsp.buf.definition, bufopts)
+  buf_set_keymap('n', 'K', vim.lsp.buf.hover, bufopts)
+  buf_set_keymap('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  buf_set_keymap('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  buf_set_keymap('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  buf_set_keymap('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  buf_set_keymap('n', '<space>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  buf_set_keymap('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  buf_set_keymap('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  buf_set_keymap('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  buf_set_keymap('n', 'gr', vim.lsp.buf.references, bufopts)
+
+  buf_set_keymap('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+
+--   -- Set some keybinds conditional on server capabilities
+--   if client.server_capabilities.document_formatting then
+--     buf_set_keymap("n", "<space>f", vim.lsp.buf.formatting(), bufopts)
+--   elseif client.server_capabilities.document_range_formatting then
+--     --buf_set_keymap('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+--     buf_set_keymap("n", "<space>f", vim.lsp.buf.formatting(), bufopts)
+--   end
+
 end
 
