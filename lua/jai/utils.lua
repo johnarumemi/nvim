56c69d55f98bb1bbdb1f3b6d09d8fa6835ea199a
John Arumemi-Ikhide <john.arumemi@gmail.com>
2023-02-07 23:21:09 +0000

    add utility function and module
    add `dump` utility function, that dumps a table into a string.
    Note that this function errors if there is a function within
    the table.

---
+++ lua/jai/utils.lua
@@ -0,0 +1,22 @@
+-- [[ utility purposes only ]]
+
+-- dump a table into a string, useful for debugging.
+-- use ':messages' to view printed out table.
+local function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+
+-- define what is publicly exported here
+return {
+  dump = dump
+}
