e4416b7f319162da9abc6a433ba1473e3d7f9644
John Arumemi-Ikhide <john.arumemi@gmail.com>
2023-02-12 22:26:12 +0000

    minor formatting

--- lua/jai/lsp/lspconfig.lua
+++ lua/jai/lsp/lspconfig.lua
@@ -11,7 +11,7 @@ local capabilities = require("jai.lsp.capabilities")
 -- Some LSP's are setup via other plugins:
 -- rust: setup by rust-tools. see jai/rust.lua for setup configuration
 
--- After setting up mason-lspconfig you may set up servers 
+-- After setting up mason-lspconfig you may set up servers
 -- via lspconfig
 -- Note: this means setup client configurations against a lsp server
 -- after all, lspconfig is just a collection of configurations.
@@ -20,46 +20,52 @@ local capabilities = require("jai.lsp.capabilities")
 
 -- Lua
 -- mason package name = lua-language-server
-require("lspconfig").sumneko_lua.setup {
-  on_attach=function(client, bufnr)
+require("lspconfig").sumneko_lua.setup({
+    on_attach = function(client, bufnr)
+      -- apply default on_attach settings
+      on_attach(client, bufnr)
 
-    -- apply default on_attach settings
-    on_attach(client, bufnr)
-
-    -- update options for lua files
-    opt.shiftwidth = 2  -- num:  Size of an indent
-    opt.softtabstop = 2 -- num:  Number of spaces tabs count for in insert mode
-    opt.tabstop = 2     -- num:  Number of spaces tabs count for
-
+      -- update options for lua files
+      opt.shiftwidth = 2 -- num:  Size of an indent
+      opt.softtabstop = 2 -- num:  Number of spaces tabs count for in insert mode
+      opt.tabstop = 2 -- num:  Number of spaces tabs count for
     end,
-    capabilities=capabilities
-}
+    capabilities = capabilities,
+})
 
 -- Python
-require("lspconfig").pyright.setup{
-    on_attach=function(client, bufnr)
+require("lspconfig").pyright.setup({
+    on_attach = function(client, bufnr)
+      -- apply default on_attach settings
+      on_attach(client, bufnr)
 
-    -- apply default on_attach settings
-    on_attach(client, bufnr)
-
-    -- update options for lua files
-    opt.shiftwidth = 4  -- num:  Size of an indent
-    opt.softtabstop = 4 -- num:  Number of spaces tabs count for in insert mode
-    opt.tabstop = 4     -- num:  Number of spaces tabs count for
-
+      -- update options for lua files
+      opt.shiftwidth = 4 -- num:  Size of an indent
+      opt.softtabstop = 4 -- num:  Number of spaces tabs count for in insert mode
+      opt.tabstop = 4 -- num:  Number of spaces tabs count for
     end,
-    capabilities=capabilities
-}
+    capabilities = capabilities,
+})
 
 -- SQL
-require("lspconfig").sqlls.setup{
-  on_attach=on_attach,
-  capabilities=capabilities
-}
+require("lspconfig").sqlls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
 
 -- Markdown
-require("lspconfig").marksman.setup{
-  on_attach=on_attach,
-  capabilities=capabilities
-}
+require("lspconfig").marksman.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
 
+require("lspconfig").eslint.setup({
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+      vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          command = "EslintFixAll",
+      })
+    end,
+})
--- lua/jai/plugins/null_ls_config.lua
+++ lua/jai/plugins/null_ls_config.lua
@@ -1,36 +1,35 @@
 -- [[ null-ls setup ]]
 -- repo: https://github.com/jose-elias-alvarez/null-ls.nvim
 
-
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-  sources = {
-    -- Lua
-    null_ls.builtins.formatting.stylua, -- An opinionated code formatter for Lua.
+    sources = {
+        -- Lua
+        null_ls.builtins.formatting.stylua, -- An opinionated code formatter for Lua.
 
-    --- JS/TS
-    null_ls.builtins.diagnostics.eslint,
+        --- JS/TS
+        null_ls.builtins.diagnostics.eslint,
 
-    -- Python
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.diagnostics.flake8,
+        -- Python
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.flake8,
 
-    -- Other
-    null_ls.builtins.completion.spell, -- Spell suggestions completion source.
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+        -- Other
+        null_ls.builtins.completion.spell, -- Spell suggestions completion source.
+    },
+    on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+        })
+      end
+    end,
 })
