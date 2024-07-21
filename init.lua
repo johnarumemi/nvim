051c68b496cecb0369ddf471892c90b0a568c695
John Arumemi-Ikhide <john.arumemi@gmail.com>
2024-07-21 20:35:18 +0100

    update bootstrap file

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
+-- Set logging level to debug
+vim.lsp.set_log_level("debug")
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("jai.config")
+
+-- Print message if VSCode neovim extension is activated
+-- and neovim confing was successfully loaded in VSCode.
+if vim.g.vscode then
+  print("VSCode neovim extension activated")
+end
