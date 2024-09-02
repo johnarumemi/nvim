b6b758f757b223e22d3c5fc7d3bcddcb6633378f
John Arumemi-Ikhide <john.arumemi-ikhide@moodys.com>
2024-09-02 11:52:25 +0100

    add comments to keymaps for persistence plugin

--- lua/jai/plugins/core/util.lua
+++ lua/jai/plugins/core/util.lua
@@ -28,8 +28,14 @@ return {
     opts = { options = vim.opt.sessionoptions:get() },
     -- stylua: ignore
     keys = {
+      { "<leader>h", group = "Persistence" },
+      -- load the session for the current directory
       { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+      -- select a session to load
+      { "<leader>qS", function() require("persistence").select() end, desc = "Select Session to restore" },
+      -- load the last session
       { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      -- stop Persistence => session won't be saved on exit
       { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
   },
