e8a58b5d0f003de7abcf079a7b6fd498bd6bf56d
John Arumemi-Ikhide <john.arumemi@gmail.com>
2024-09-07 14:03:29 +0100

    add todo-comments plugin

--- lua/jai/plugins/core/editor.lua
+++ lua/jai/plugins/core/editor.lua
@@ -146,4 +146,23 @@ return {
       { "[[", desc = "Prev Reference" },
     },
   },
+  {
+    "folke/todo-comments.nvim",
+    cmd = { "TodoTrouble", "TodoTelescope" },
+
+    -- event = "LazyFile",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    dependencies = { "folke/which-key.nvim" },
+    opts = {},
+
+    -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find Todo All" },
+      { "<leader>fa", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    },
+  },
 }
