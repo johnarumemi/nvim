efe102ec080f909c2ed3b37f86944fa61e417a90
John Arumemi-Ikhide <john.arumemi@gmail.com>
2024-12-02 10:32:54 +0000

    feat(snippets): set snippets path on initialisation

--- init.lua
+++ init.lua
@@ -11,6 +11,10 @@ vim.debug("neorg environment: " .. _G.neorg_env, { title = "Init" })
 -- set colorscheme using custom command
 vim.cmd("ColorschemeAuto rose-pine")
 
+local snippet_search = vim.fn.stdpath("config") .. "/snippets"
+
+vim.debug("snippets path: " .. snippet_search, { title = "Init" })
+
 -- Print message if VSCode neovim extension is activated
 -- and neovim confing was successfully loaded in VSCode.
 if vim.g.vscode then
