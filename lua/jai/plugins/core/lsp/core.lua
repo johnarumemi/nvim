a5ff2c5ba4f3ee65550f8e2ad1481b160680a0e4
John Arumemi-Ikhide <john.arumemi@gmail.com>
2025-05-26 00:10:38 +0100

    update mason to append to PATH rather than prepend

--- lua/jai/plugins/core/lsp/core.lua
+++ lua/jai/plugins/core/lsp/core.lua
@@ -135,6 +135,13 @@ return {
       end
 
       return {
+        ---@since 1.0.0
+        -- Where Mason should put its bin location in your PATH. Can be one of:
+        -- - "prepend" (default, Mason's bin location is put first in PATH)
+        -- - "append" (Mason's bin location is put at the end of PATH)
+        -- - "skip" (doesn't modify PATH)
+        ---@type '"prepend"' | '"append"' | '"skip"'
+        PATH = "append",
         ensure_installed = base,
       }
     end,
